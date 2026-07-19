1cb4afe9b13c62d54f286ad77bacb380e55585aa
Ashutosh <guptaashutosh9918@gmail.com>
2026-07-19 19:06:43 +0530

    sixth commit

--- models/user.js
+++ models/user.js
@@ -1,14 +1,29 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    _id:{ type : String , required:true},
-    name:{type :String , required: true},
-    email:{type :String , required: true , unique:true},
-    imageUrl : {type : String, required :true},
-    cartItems : {type:Object, default:{}},
-    minimize:false
-})
+    _id: { 
+        type: String, 
+        required: true 
+    },
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    imageUrl: { 
+        type: String, 
+        required: true 
+    },
+    cartItems: { 
+        type: Object, 
+        default: {} 
+    }
+});
 
-const User = mongoose.models.user || mongoose.model('user',userSchema)
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 
-export default User 
+export default User;
